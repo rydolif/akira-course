6a1e46d9c5d24239c88fc9b2e203339eb377b1ce
master <rudolifrudolif@gmail.com>
2020-04-10 12:58:36 +0300

    pravki 3.1 img + link + number js commit

--- app/js/common.js
+++ app/js/common.js
@@ -137,23 +137,25 @@ $(function() {
   $('.modal').popup({transition: 'all 0.3s'});
 
 //-------------------------------анімація цифр---------------------------------------
-  var show = true;
-  var countbox = ".number";
-  $(window).on("scroll load resize", function () {
-      if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
-      var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
-      var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
-      var w_height = $(window).height(); // Высота окна браузера
-      var d_height = $(document).height(); // Высота всего документа
-      var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
-      if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
-          $('.number__item b').spincrement({
-              thousandSeparator: "",
-              duration: 3000
-          });
-          show = false;
-      }
-  });
+   if($(".number__item").hasClass('number__item')){
+      var show = true;
+      var countbox = ".number";
+      $(window).on("scroll load resize", function () {
+          if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+          var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+          var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+          var w_height = $(window).height(); // Высота окна браузера
+          var d_height = $(document).height(); // Высота всего документа
+          var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+          if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+              $('.number__item b').spincrement({
+                  thousandSeparator: "",
+                  duration: 3000
+              });
+              show = false;
+          }
+      });
+    }
 
 
   if(document.documentElement.clientWidth <= 1580) {
